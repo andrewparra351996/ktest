dbb7a443481a7da552233e8a446d0be1753e5ed2
andrewparra351996 <59966610+andrewparra351996@users.noreply.github.com>
2020-01-17 14:04:23 -0500

    Create run_chrome.sh

---
+++ run_chrome.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rfw ./reports/chrome
+mkdir -p ./reports/chrome
+
+katalon_opts='-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/Pass_Fail_Suite"'
+docker run --rm -v $(pwd)/katalontestscripts:/katalon/katalon/source:ro -v $(pwd)/reports/chrome:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
