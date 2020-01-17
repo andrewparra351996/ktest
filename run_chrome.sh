a311fc1d29db481eaf729cd2acc28a663445a709
andrewparra351996 <59966610+andrewparra351996@users.noreply.github.com>
2020-01-17 15:29:33 -0500

    Update run_chrome.sh

--- run_chrome.sh
+++ run_chrome.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -rfv ./reports/chrome
-mkdir -p ./reports/chrome
-
 set -xe
 
 ksversion=$1
 apiKey=$2
+
+rm -rfv ./reports/chrome
+mkdir -p ./reports/chrome
 
 docker run -t --rm  -e KATALON_USER_ID=`id -u $USER` -v c:/users/parraa/Katalon\ Studio/katalontestscripts:/katalon/katalon/source -v c:/users/parraa/Katalon\ Studio/reports/chrome:/katalon/katalon/report katalonstudio/katalon:$ksversion katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test\ Suites/Pass_Fail_Suite" -apiKey=${apiKey}
