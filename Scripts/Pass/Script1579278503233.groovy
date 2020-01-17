a32fc06e97a53917d2a7af4d419057a4f2fbc2fe
parraa <parraa@host.docker.internal>
2020-01-17 11:30:43 -0500

    Commit Katalon scripts

---
+++ Scripts/Pass/Script1579278503233.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.gamestop.com/')
+
+WebUI.click(findTestObject('Object Repository/Page_Consoles Collectibles Video Games amp _d911fd/span_Video Games'))
+
+WebUI.click(findTestObject('Object Repository/Page_Consoles Collectibles Video Games amp _d911fd/img_Video Games_category-thumb ae-img'))
+
+WebUI.click(findTestObject('Object Repository/Page_Consoles Collectibles Video Games amp _d911fd/a_New Releases'))
+
+WebUI.click(findTestObject('Object Repository/Page_PS4 Pro  Slim - Buy PS4 Games Consoles_ef21d4/a_Terminator Resistance'))
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/Pass_Fail_Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Pass_Fail_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>66bc78e5-7273-40a2-aeee-d932a7eb78da</testSuiteGuid>
+</TestSuiteEntity>
