dac96c82c1a0fd1ff560cc6c2b3955d3697d0d58
parraa <parraa@host.docker.internal>
2020-01-22 14:42:44 -0500

    Update openBrowser

--- Test Suites/Pass_Or_Fail.groovy
+++ Test Suites/Pass_Or_Fail.groovy
@@ -49,7 +49,7 @@ def tearDown() {
  */
 @SetupTestCase(skipped = false) // Please change skipped to be false to activate this method.
 def setupTestCase() {
-	WebUI.openBrowser(null)
+	WebUI.openBrowser('')
 }
 
 /**
